d75e57fcb2164ed05f097f707a7e4c8a9508761b
Rahul-6265 <rahulrc0907@gmail.com>
2022-01-02 17:47:27 +0530

    assign1

---
+++ 1stAssignment.js
@@ -0,0 +1,8 @@
+//Write a program to check whether the number is greater than 10 or not.
+
+var num=9;
+if(num > 10){
+    console.log("Number is greater than 10")
+} else{
+    console.log("Number is less than 10")
+}
